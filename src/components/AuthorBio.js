81b166e25a7dc513202b024a7aea59bc1ea999d5
Fernando Papaqui <hola@rivaslevi.mx>
2021-02-14 19:58:25 -0600

    add author bio

--- src/components/AuthorBio.js
+++ src/components/AuthorBio.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import layoutStyles from './layout.module.scss'
 
 export const AuthorFragment = graphql`
   fragment AuthorBio on WpUser {
@@ -11,7 +12,7 @@ export const AuthorFragment = graphql`
 const AuthorBio = ({ author: { id, name, description } }) => {
   return (
     <div className={`author-${id}`}>
-      <h2>{name}</h2>
+      <h2 className={layoutStyles.authorName}>{name}</h2>
       <div dangerouslySetInnerHTML={{ __html: description }} />
     </div>
   )
